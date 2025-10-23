da89cb43628f191c866cf720e113a323684d76e4
Arun <nalabalaaruneswarreddy2601@gmail.com>
2025-10-23 18:44:15 +0530

    Add 'webp' format to allowed storage formats

--- cloudConfig.js
+++ cloudConfig.js
@@ -13,7 +13,7 @@ const storage = new CloudinaryStorage({
   cloudinary: cloudinary,
   params: {
     folder: 'quotify_DEV',
-    allowed_formats: ['jpg','png','jpeg'],
+    allowed_formats: ['jpg','png','jpeg','webp'],
   },
   
 });
